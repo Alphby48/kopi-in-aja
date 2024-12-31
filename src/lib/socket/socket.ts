b0efe56cd39f9b6fd33402c2deb89fccd461a870
Alphby48 <hazerdmobile@gmail.com>
2024-12-31 23:41:08 +0700

    hadeh

--- src/lib/socket/socket.ts
+++ src/lib/socket/socket.ts
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const SOCKET_URL = "https://kopiinaja-zeta.vercel.app";
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 export const socket = io(SOCKET_URL, {
   path: "/api/socket",
   autoConnect: false, // Supaya tidak langsung connect
